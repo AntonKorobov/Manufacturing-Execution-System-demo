c0cebb18b4e6740a23de342b4562d508d9212b0b
NewAnton <anton1korobov1@gmail.com>
2024-01-14 19:13:13 +0100

    feat: connect app to blob data base

---
+++ frontend/src/app/images-from-db/page.tsx
@@ -0,0 +1,9 @@
+import { ImagesFromDB } from '@/components/ImagesFromDB/ImagesFromDB';
+
+export default function ImagesFromDBPage() {
+  return (
+    <div>
+      <ImagesFromDB />
+    </div>
+  );
+}
