b9999c1d439e7666f2166baed9c3192b542f2c40
NewAnton <anton1korobov1@gmail.com>
2024-01-28 14:48:01 +0100

    feat: update create-log function

--- frontend/src/app/api/create-log/route.ts
+++ frontend/src/app/api/create-log/route.ts
@@ -2,6 +2,8 @@ export const revalidate = 0;
 
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function PUT(req: NextRequest) {
-  return NextResponse.json(req, { status: 200 });
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  return NextResponse.json(body.event.data.new, { status: 200 });
 }
