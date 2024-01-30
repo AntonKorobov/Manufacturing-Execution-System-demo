277406bfc973adb34e0b9b4932d85f13e0da13d3
NewAnton <anton1korobov1@gmail.com>
2024-01-30 21:23:11 +0100

    fix: serverless function logic

--- frontend/src/app/api/update-job-status/route.ts
+++ frontend/src/app/api/update-job-status/route.ts
@@ -6,12 +6,15 @@ import { GET_JOB_OPERATIONS } from '@/graphQL/queries';
 import { OperationStatuses, getJobOperationsResponse } from '@/graphQL/types';
 import { PUT_JOB_STATUS } from '@/graphQL/mutations';
 
-export async function POST(req: NextRequest) {
+export async function PUT(req: NextRequest) {
   const body = await req.json();
   const { job_id } = body.event.data.new;
 
   try {
-    const response = getJobStations(job_id).then((data) => postJobStatus(data));
+    const response = await getJobStations(job_id).then((operations) => {
+      const newStatusCode = calculateStatusCode(operations);
+      return postJobStatus({ jobId: job_id, statusCode: newStatusCode });
+    });
 
     if (response.errors) {
       return NextResponse.json(
@@ -44,45 +47,53 @@ async function getJobStations(jobId: number) {
       'content-type': 'application/json',
       'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET as string,
     },
-    body: JSON.stringify(GET_JOB_OPERATIONS({ jobId: jobId })), //fetch operations statuses
+    body: JSON.stringify({
+      query: GET_JOB_OPERATIONS({ jobId: jobId }),
+    }),
   }).then((data) => data.json());
 
-  return response;
+  return response.data.job_operation;
 }
 
 async function postJobStatus({
-  operations,
+  statusCode,
   jobId,
 }: {
-  operations: getJobOperationsResponse;
+  statusCode: number;
   jobId: number;
 }) {
-  let newStatusCode = 4;
-
-  for (let i = 0; i < operations.length; i++) {
-    if (
-      operations[i].operation_status.operation_status_name ===
-      OperationStatuses.IN_PROGRESS
-    ) {
-      newStatusCode = 3;
-      break;
-    }
-    if (
-      operations[i].operation_status.operation_status_name === OperationStatuses.QUEUED
-    ) {
-      newStatusCode = 2;
-      break;
-    }
-  }
-
   const response = await fetch(process.env.HASURA_PROJECT_ENDPOINT as string, {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
       'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET as string,
     },
-    body: JSON.stringify(PUT_JOB_STATUS({ id: jobId, statusCode: newStatusCode })),
+    body: JSON.stringify({
+      query: PUT_JOB_STATUS({ id: jobId, statusCode: statusCode }),
+    }),
   }).then((data) => data.json());
 
   return response;
+}
+
+function calculateStatusCode(operations: getJobOperationsResponse) {
+  let statusCode = 4;
+
+  for (let i = 0; i < operations.length; i++) {
+    if (
+      operations[i].operation_status.operation_status_name ===
+      OperationStatuses.IN_PROGRESS
+    ) {
+      statusCode = 3;
+      break;
+    }
+    if (
+      operations[i].operation_status.operation_status_name === OperationStatuses.QUEUED
+    ) {
+      statusCode = 2;
+      break;
+    }
+  }
+
+  return statusCode;
 }
